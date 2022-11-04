b23d6a21ecf979fea2128f51f362312ea62e1cb0
“Carlos <carlos.valenzh@gmail.com>
2022-11-03 19:12:33 -0600

    Mod finales

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -7,19 +7,21 @@ use Illuminate\Http\Request;
 use App\Models\Zip_code;
 use App\Http\Resources\ZipCode;
 use Illuminate\Support\Facades\DB;
-
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class ApiController extends Controller
 {
 
-    public function index($zc)
+    public function index($zc) : JsonResource
     {
         //buscamos en la base de datos para que le sean enviados los datos al Resources
         $zip = DB::table('zip_codes')->where('d_codigo',$zc)->get();
         if (empty($zip)){
             abort(404);
         }
-        return new ZipCode($zip);
+
+    $zip_code = new ZipCode(json_decode($zip->toArray())->data);
+        return json_decode($zip_code)    ;
 
     }
 }
--- app/Http/Resources/ZipCode.php
+++ app/Http/Resources/ZipCode.php
@@ -17,24 +17,22 @@ class ZipCode extends ResourceCollection
     {
         $zip = $this->collection;
         //obtenemos la informacion de la base de datos y empezamos a organizar el json mediante arrays
-        $data= array("zip_code" => $zip[0]->d_codigo,
+        $zp= array("zip_code" => $zip[0]->d_codigo,
             "locality" => $zip[0]->d_estado,
             "federal_entity" => array("key" => $zip[0]->c_estado,
                 "name" => $zip[0]->d_ciudad,
                 "code" => $zip[0]->c_CP));
         //lo ingresamos a un for por los codigos postales que tienen más asentamientos
         for ($i = 0, $long = count($zip); $i < $long; ++$i) {
-            $data1[$i] = array("key" => $zip[$i]->id_asenta_cpcons,
+            $zp1[$i] = array("key" => $zip[$i]->id_asenta_cpcons,
                 "name" => $zip[$i]->d_asenta,
                 "zona_type" => $zip[$i]->d_zona,
                 "senttlement_type" => array("name" => $zip[$i]->d_tipo_asenta));
         };
         //continuamos obteniendo la data en otro array
-        $data2=array("municipality" => array("key" => $zip[0]->c_mnpio, "name" => $zip[0]->D_mnpio));
-        //creamos una variable con las arrays para imprimirlo
-        $zip_code = $data + array("settlements"=>$data1) + $data2;
+        $zp2=array("municipality" => array("key" => $zip[0]->c_mnpio, "name" => $zip[0]->D_mnpio));
         //mandamos como reponse el json de la información obtenida de la base de dato
-
+        $zip_code= $zp + array("settlements"=>$zp1) + $zp2;
         return $zip_code;
     }
 }
